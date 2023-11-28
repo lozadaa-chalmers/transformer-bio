9044a0cea21691bfd5fb16dfff9669ed6d575368
ronjakh <ronjah@student.chalmers.se>
2023-11-28 13:58:50 +0100

    Added comments to functions in cell_classification.py

--- tasks/data_handling/cell_classification.py
+++ tasks/data_handling/cell_classification.py
@@ -6,7 +6,8 @@ from typing import Literal
 def classify_cells(pre_processed_adata: sc.AnnData = None,
                    class_df: pd.DataFrame = None,
                    classification_key: str = 'suva_class',
-                   save_path: str = 'classified_temp.h5'
+                   save_path: str = 'classified_temp.h5',
+                   store_classification_score: bool = True
                    ) -> sc.AnnData:
     """
     ### Function that classifies cells and saves the result as a h5-file.
@@ -19,6 +20,8 @@ def classify_cells(pre_processed_adata: sc.AnnData = None,
         - classification_key (str): Key to stored classification result in pre_processed_adata.obs.
             Default 'suva_class'
         - save_path (str): Save path for h5-file, include file name. Default 'classified_temp.h5'
+        - store_classification_score (bool): If True the scores of each class is included in the h5-file, if False
+            only the classification is included without the scores. Default True
 
     #### Returns:
         - AnnData: With class scores and classification
@@ -38,15 +41,33 @@ def classify_cells(pre_processed_adata: sc.AnnData = None,
                         unique_categories_list=categories,
                         classification_key=classification_key)
 
-    # Save the AnnData object to an h5 file
+    if not store_classification_score:
+        adata_temp = pre_processed_adata.copy()
+        adata_temp.obs[classification_key] = adata.obs[classification_key]
+        adata = adata_temp
+
+    # Save the AnnData object to a h5 file
     adata.write_h5ad(save_path)
     return adata
 
 
 def score_categories(pre_processed_adata: sc.AnnData = None,
-                     unique_categories_list: list = [],
-                     class_df: pd.DataFrame = ''
+                     unique_categories_list: list = None,
+                     class_df: pd.DataFrame = None
                      ):
+    """
+    ### Function that scores categories in the AnnData object
+
+    ---
+
+    #### Args:
+        - pre_processed_adata (AnnData): AnnData object that has gone through pre-processing. Default None
+        - unique_categories_list (list): List with the unique categories. Default None
+        - class_df (DataFrame): DataFrame with categories 'genes' and 'set'. Default None
+
+    ---
+    Written: ronjah@chalmers.se
+    """
     for suva_class in unique_categories_list:
         gene_list = class_df.loc[class_df['set'] == suva_class, 'genes'].tolist()
         sc.tl.score_genes(pre_processed_adata, score_name=suva_class, gene_list=gene_list)
@@ -54,9 +75,23 @@ def score_categories(pre_processed_adata: sc.AnnData = None,
 
 
 def annotate_categories(scored_adata: sc.AnnData = None,
-                        unique_categories_list: list = [],
+                        unique_categories_list: list = None,
                         classification_key: str = 'suva_class'
                         ):
+    """
+    ### Function that annotates the class with the highest score for each cell
+
+    ---
+
+    #### Args:
+        - scored_adata (AnnData): AnnData object that has scored classes. Default None
+        - unique_categories_list (list): List with the unique categories. Default None
+        - classification_key (str): Key to stored classification result in pre_processed_adata.obs.
+            Default 'suva_class'
+
+    ---
+    Written: ronjah@chalmers.se
+    """
     # Calculate max value in each cell
     max_class_column = scored_adata.obs[unique_categories_list].idxmax(axis=1)
 
