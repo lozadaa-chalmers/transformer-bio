70378d6937830ddc1c87904bff1b35963c0e5e66
ronjakh <ronjah@student.chalmers.se>
2023-12-07 15:35:27 +0100

    Made the changes requested

--- tasks/data_handling/tests/test_cell_classification.py
+++ tasks/data_handling/tests/test_cell_classification.py
@@ -17,9 +17,9 @@ class TestScoreCategories(unittest.TestCase):
     adata = sc.AnnData(X=X_data)
     adata.obs_names = ['a', 'b', 'c']
     adata.var_names = ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9']
-    cell_classification.score_categories(pre_processed_adata=adata,
-                                         unique_categories_list=unique_categories_list,
-                                         class_df=class_df)
+    adata_scored = cell_classification.score_categories(pre_processed_adata=adata,
+                                                        unique_categories_list=unique_categories_list,
+                                                        class_df=class_df)
     c1_test = np.zeros((3, 1))
     c2_test = np.zeros((3, 1))
     c3_test = np.zeros((3, 1))
@@ -33,15 +33,15 @@ class TestScoreCategories(unittest.TestCase):
 
     def test_c1_correct(self):
         with self.assertRaises(ValueError):
-            self.assertEqual(self.adata.obs.c1, self.c1_test)
+            self.assertEqual(self.adata_scored.obs.c1, self.c1_test)
 
     def test_c2_correct(self):
         with self.assertRaises(ValueError):
-            self.assertEqual(self.adata.obs.c2, self.c2_test)
+            self.assertEqual(self.adata_scored.obs.c2, self.c2_test)
 
     def test_c3_correct(self):
         with self.assertRaises(ValueError):
-            self.assertEqual(self.adata.obs.c3, self.c3_test)
+            self.assertEqual(self.adata_scored.obs.c3, self.c3_test)
 
 
 class TestAnnotateCategories(unittest.TestCase):
@@ -53,13 +53,14 @@ class TestAnnotateCategories(unittest.TestCase):
     adata = sc.AnnData(X=X_data)
     adata.obs_names = ['a', 'b', 'c']
     adata.var_names = ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9']
-    cell_classification.score_categories(pre_processed_adata=adata,
-                                         unique_categories_list=unique_categories_list,
-                                         class_df=class_df)
-    cell_classification.annotate_categories(scored_adata=adata,
-                                            unique_categories_list=unique_categories_list)
-    max_class_column = adata.obs[unique_categories_list].idxmax(axis=1)
+    adata_scored = cell_classification.score_categories(pre_processed_adata=adata,
+                                                        unique_categories_list=unique_categories_list,
+                                                        class_df=class_df)
+    adata_classified = cell_classification.annotate_categories(scored_adata=adata_scored,
+                                                               unique_categories_list=unique_categories_list)
+
+    max_class_column = adata_classified.obs[unique_categories_list].idxmax(axis=1)
 
     def test_classes_correct(self):
         with self.assertRaises(ValueError):
-            self.assertEqual(self.adata.obs.suva_class, self.max_class_column)
+            self.assertEqual(self.adata_classified.obs.suva_class, self.max_class_column)
