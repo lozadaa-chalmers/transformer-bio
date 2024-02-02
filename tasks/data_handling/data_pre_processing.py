288eecea023a0ca4e6d9fb891a0cc6b5be0a44d5
Alejandro Lozada Cortes <lozadaa@chalmers.se>
2024-02-02 18:00:34 +0100

    make genes unique in **kwargs of the preprocessing

--- tasks/data_handling/data_pre_processing.py
+++ tasks/data_handling/data_pre_processing.py
@@ -6,7 +6,8 @@ import scanpy as sc
 def create_count_matrix(
         file_path: str = None,
         genome: str = None,
-        make_genes_unique: bool = False
+        make_genes_unique: bool = False,
+        **kwargs
 ) -> sc.AnnData:
     """
     ### Function that transforms 10x h5-file to a count matrix.
@@ -208,7 +209,8 @@ def pre_process_data_pipeline(
         n_top_genes: int = None,
         min_n_cells: int = 50,
         n_top_plot: int = 20,
-        save_format: str = 'png'
+        save_format: str = 'png',
+        **kwargs
 ) -> sc.AnnData:
     """
     ### Function that acts as a pipeline for pre-processing the data.
@@ -257,7 +259,8 @@ def pre_process_data_pipeline(
     if file_path is None:
         raise ValueError('Need a path to h5-file.')
 
-    adata = create_count_matrix(file_path)
+    adata = create_count_matrix(file_path,
+                                make_genes_unique=kwargs.get('make_genes_unique', None))
 
     if plots:
         allowed_formats = ['png', 'pdf', 'svg']
